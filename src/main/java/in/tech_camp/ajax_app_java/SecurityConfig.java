4df96f2f74b228e3c49366a4fa1c06c98efee53d
hana222k <hanae.yazawa@smart-group.co.jp>
2026-06-11 11:39:49 +0900

    セキュリティの導入

---
+++ src/main/java/in/tech_camp/ajax_app_java/SecurityConfig.java
@@ -0,0 +1,50 @@
+package in.tech_camp.ajax_app_java;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@EnableWebSecurity
+public class SecurityConfig {
+  @Bean
+  // securityFilterChainメソッドでアプリのセキュリティ設定
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+      // authorizeHttpRequests()：全てのリクエストに対して認証が必要という設定
+      .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+        .anyRequest().authenticated()
+      )
+      // ログイン画面へのアクセスを許可
+      .formLogin(form -> form
+        .permitAll() 
+      )
+      // 本アプリでBasic認証を使用する
+      .httpBasic(httpBasicCustomizer -> { });
+
+    return http.build();
+  }
+  
+  @Bean
+  // userDetailsServiceメソッドでBasic認証に使用するユーザー情報の指定
+  public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+    UserDetails user = User.withUsername("admin")
+        .password(encoder.encode("2222"))
+        .roles("ADMIN")
+        .build();
+    return new InMemoryUserDetailsManager(user);
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+}
