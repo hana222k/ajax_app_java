63d153bf647642925ebf35abd00b554d5f462089
hana222k <hanae.yazawa@smart-group.co.jp>
2026-06-11 13:37:25 +0900

    環境変数にユーザー名とパスワードの設定

--- src/main/java/in/tech_camp/ajax_app_java/SecurityConfig.java
+++ src/main/java/in/tech_camp/ajax_app_java/SecurityConfig.java
@@ -36,8 +36,11 @@ public class SecurityConfig {
   @Bean
   // userDetailsServiceメソッドでBasic認証に使用するユーザー情報の指定
   public UserDetailsService userDetailsService(PasswordEncoder encoder) {
-    UserDetails user = User.withUsername("admin")
-        .password(encoder.encode("2222"))
+    String username = System.getenv("BASIC_AUTH_USER");
+    String password = System.getenv("BASIC_AUTH_PASSWORD");
+
+    UserDetails user = User.withUsername(username)
+        .password(encoder.encode(password))
         .roles("ADMIN")
         .build();
     return new InMemoryUserDetailsManager(user);
